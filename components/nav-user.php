eb3198d59096a749814cdb0d93fdee8e00d04627
Muhammad Junaidi <emje662@gmail.com>
2025-11-04 17:27:15 +0700

    refactor: pisahkan navbar ke dalam komponen terpisah

---
+++ components/nav-user.php
@@ -0,0 +1,9 @@
+<!-- Navigasi -->
+<nav class="main-nav">
+	<ul>
+		<li><a href="#">Berita</a></li>
+		<li><a href="#">Teknologi</a></li>
+		<li><a href="#">Ekonomi</a></li>
+		<li><a href="#">Politik</a></li>
+	</ul>
+</nav>
