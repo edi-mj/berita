057904620857cc94fa862505aca61700295c6a08
Muhammad Junaidi <emje662@gmail.com>
2025-11-04 17:20:06 +0700

    chore: tambah konfigurasi langsung ke file conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,9 @@
 <?php
-require_once 'config.php';
+// Konfigurasi database
+define("HOST", "localhost");
+define("USER", "root");
+define("PASS", "");
+define("DBNAME", "berita");
 
 // Database Source Name
 const DSN = "mysql:host=" . HOST . ";dbname=" . DBNAME;
@@ -15,4 +19,5 @@ try {
   define("DBH", new PDO(DSN, USER, PASS, OPTIONS));
 } catch (PDOException $err) {
   echo "Koneksi Gagal: " . $err->getMessage();
+  die();
 }
